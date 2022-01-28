026383abccfd1ceb004d03a560275b9d5d14cbd8
TheZenMasterz <56887776+TheZenMasterz@users.noreply.github.com>
2022-01-28 12:36:48 -0800

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -13,10 +13,9 @@ public class MarkdownParse {
         while(currentIndex < markdown.length()) {
             int nextOpenBracket = markdown.indexOf("[", currentIndex);
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
-
-            /*if(nextCloseBracket == 1 || nextOpenBracket == -1){
+            if(nextCloseBracket == 1 || nextOpenBracket == -1){
                 return toReturn;
-            }*/
+            }
             int openParen = markdown.indexOf("(", nextCloseBracket);
             int closeParen = markdown.indexOf(")", openParen);
             if(closeParen == -1){
